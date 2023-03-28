39de68ccfd423257a009091586ffd7464b550a5c
dharaneesh227 <d20z136@psgitech.ac.in>
2023-03-28 22:59:53 +0530

    flask test

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask,render_template,redirect,request,session,flash
-import cv2
 
+from pymongo import MongoClient
+
+app=Flask(__name__) 
+
+client = MongoClient('127.0.0.1', 27017)
+
+db = client.flask_db
+
+users = db.users
+
+returns = users.find()
+
+print(returns)
