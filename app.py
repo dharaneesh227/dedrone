1178c29b2866eb281697b02c7d9117dc92b94b1b
dharaneesh227 <d20z136@psgitech.ac.in>
2023-03-29 13:14:16 +0530

    d

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import os
 import webbrowser
 from pymongo import MongoClient
 
+import analytic as an
+
 app=Flask(__name__)
 
 client = MongoClient('127.0.0.1', 27017)
 
-db = client['dedrone']
+db = client.get_database('dedrone')
 users = db.users
 
 # returns = users.insert_one({"sample":"test 1"}).inserted_id
@@ -22,14 +24,14 @@ def overview():
     return render_template("overview.html")
 
 @app.route("/login")
-def login():
+def loging():
     return render_template("login.html")
 
 @app.get("/availability")
 def availability():
     return render_template("availability.html")
 
-@app.post("/business")
+@app.route("/business",methods = ['POST','GET'] )
 def business():
     return render_template("business.html")
 
@@ -42,9 +44,66 @@ def potentialuser():
     return render_template("potentialuser.html")
 
 
-@app.get("/tracking")
+@app.get("/tracking") 
 def tracking():
     return render_template("tracking.html")
+
+@app.post("/register")
+def regp():
+    if request.method == "POST":
+        name = request.form.get("Name")
+        org = request.form.get("Org")
+        gst = request.form.get("GSTno")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        print("\n\n"+name+org+gst+email+password+"\n\n")
+        # user_found = users.find_one({"name": name}
+
+        # if user_found:
+        #         return redirect("/")
+        #         # message = 'There already is a user by that name'
+        #         # return render_template('index.html')
+        #         #, message=message)
+        # if email_found:
+        #         return redirect("/")
+        #         # message = 'This email already exists in database'
+        #         # return render_template('index.html')
+        #         #, message=message)
+        # else:
+        user_input = {'name': name, 'email': email, 'password': password,'org':org,
+                           'gst':gst}
+        users.insert_one(user_input)
+        return redirect("/")
+
+# @app.post("/login")
+# def loginp():
+#     message = 'Please login to your account'
+#     if "email" in session:
+#         return redirect(url_for("logged_in"))
+
+#     if request.method == "POST":
+#         email = request.form.get("email")
+#         password = request.form.get("password")
+
+       
+#         email_found = records.find_one({"email": email})
+#         if email_found:
+#             email_val = email_found['email']
+#             passwordcheck = email_found['password']
+            
+#             if bcrypt.checkpw(password.encode('utf-8'), passwordcheck):
+#                 session["email"] = email_val
+#                 return redirect(url_for('logged_in'))
+#             else:
+#                 if "email" in session:
+#                     return redirect(url_for("logged_in"))
+#                 message = 'Wrong password'
+#                 return render_template('login.html', message=message)
+#         else:
+#             message = 'Email not found'
+#             return render_template('login.html', message=message)
+#     return render_template('login.html', message=message)
+
 
 
 if __name__ == "__main__":
